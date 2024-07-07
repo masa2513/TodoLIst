7140ecb324380508059cc189169dadf21bc2ae20
masa <massanndesu52@gmail.com>
2024-07-07 22:56:24 +0900

    変更した

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
+import { useState } from "react";
+import TodoList from "./TodoList"
 
 function App() {
+  const [todos, setTodos] = useState(["Todo1", "Todo2"])
   return (
-    <div className="App">
-    
-    </div>
+    <>
+    <TodoList todos={todos}/>
+      <input type="text" />
+      <button>タスクを追加</button>
+      <button>完了したタスクの削除</button>
+      <div>残りのタスク;0</div>
+    </>
   );
 }
 
